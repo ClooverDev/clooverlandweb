8456d689da608f9a469f8044fb9feea1dbcfde18
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-03-02 15:04:55 -0300

    Update progress_bar.js

--- javascripts/progress_bar.js
+++ javascripts/progress_bar.js
@@ -7,27 +7,34 @@ const colors = [
 	"lime",
 ]
 
-document.getElementById("cool_bar").addEventListener('click', updateBar);
+const bar = "pc_bar";
+const percentage = "pc_text"
+
+document.getElementById(bar).addEventListener('click', updateBar);
 
 function updateBar() {
-	document.getElementById("text").innerHTML = bar_amount+"%";
-	document.getElementById("cool_bar").style.width = bar_amount+"%";
+	document.getElementById(bar).style.width = bar_amount+"%";
+	document.getElementById(percentage).innerHTML = bar_amount+"%";
 	
 	if (bar_amount <= 25) {
-		document.getElementById("cool_bar").style.backgroundColor = colors[0];
-		document.getElementById("text").style.color = colors[0];
+		document.getElementById(bar).style.backgroundColor = colors[0];
+		document.getElementById(percentage).style.color = colors[0];
+		
 	} else if (bar_amount > 25 && bar_amount <= 49) {
-		document.getElementById("cool_bar").style.backgroundColor = colors[1];
-		document.getElementById("text").style.color = colors[1];
+		document.getElementById(bar).style.backgroundColor = colors[1];
+		document.getElementById(percentage).style.color = colors[1];
+		
 	} else if (bar_amount > 49 && bar_amount <= 74) {
-		document.getElementById("cool_bar").style.backgroundColor = colors[2];
-		document.getElementById("text").style.color = colors[2];
+		document.getElementById(bar).style.backgroundColor = colors[2];
+		document.getElementById(percentage).style.color = colors[2];
+		
 	} else if (bar_amount > 74 && bar_amount <= 99) {
-		document.getElementById("cool_bar").style.backgroundColor = colors[3];
-		document.getElementById("text").style.color = colors[3];
+		document.getElementById(bar).style.backgroundColor = colors[3];
+		document.getElementById(percentage).style.color = colors[3];
+		
 	} else if (bar_amount == 100) {
-		document.getElementById("cool_bar").style.backgroundColor = "lime";
-		document.getElementById("text").style.color = colors[4];
+		document.getElementById(bar).style.backgroundColor = "lime";
+		document.getElementById(percentage).style.color = colors[4];
 	}
 }
 
