03df0b8d4820031ecb9d2915fdc3e2e5d96f1281
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-03-05 13:18:42 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -149,3 +149,15 @@ for (var i = 0; i < damiao.length; i++) {
 for (var i = 0; i < forums.length; i++) {
     forums[i].addEventListener('click', moveTo__forums, false);
 }
+
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+	console.log("user is on mobile!");
+	window.location.replace("https://m.tophatgames.net");
+} else {
+	console.log("user is on desktop!");
+}
