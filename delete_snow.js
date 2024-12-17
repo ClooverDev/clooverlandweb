0ad27e341d6f54b783517e278eb545849c236bf5
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2024-12-16 21:35:47 -0300

    snow removal on mobile

---
+++ delete_snow.js
@@ -0,0 +1,8 @@
+function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+	$(".snow").remove();
+}
