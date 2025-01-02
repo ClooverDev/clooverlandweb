228a1f3482af6758aeb47199ebda02931a04f90a
ClooverDev <130509634+ClooverDev@users.noreply.github.com>
2025-01-02 19:59:10 -0300

    Delete delete_snow.js

--- delete_snow.js
+++
@@ -1,8 +0,0 @@
-function isMobile() {
-  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-  return regex.test(navigator.userAgent);
-}
-
-if (isMobile()) {
-	$(".snow").remove();
-}
